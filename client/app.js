d07033f894f74030507f966eee7d30b698113a04
Alireza Shabanzadeh <43717527+ImTheAlireza@users.noreply.github.com>
2026-02-12 02:11:31 +0330

    Add files via upload

--- client/app.js
+++ client/app.js
@@ -33,14 +33,14 @@ function getCleanExtPath() {
     var raw = cs.getSystemPath(SystemPath.EXTENSION);
     var clean = raw.replace(/\\/g, '/');
     
-    // Remove file:// or file:/// prefix
+
     if (clean.indexOf('file:///') === 0) {
         clean = clean.replace('file:///', '');
     } else if (clean.indexOf('file://') === 0) {
         clean = clean.replace('file://', '');
     }
     
-    // Remove trailing slash
+
     if (clean.charAt(clean.length - 1) === '/') {
         clean = clean.substring(0, clean.length - 1);
     }
